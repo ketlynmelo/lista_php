0b4d9d39655ad2426034a613fd6bd78dc8ebd897
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-07-08 08:25:33 -0300

    Fix - Arrumei um erro no codigo do exercicio 4

--- ex_04.php
+++ ex_04.php
@@ -1,28 +1,25 @@
 <?php
 
 function gerarSenha($digitos) {
-    //todos os conjuntos possiveis para a senha
-    $maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $minusculas = 'abcdefghijklmnopqrstuvwxyz';
-    $numeros = '0123456789';
-    $especiais = '!@#$%^&*()-_=+[]{}|;:,.<>?';
 
-    // Juntas todos os caracteres em um único conjunto
-    $todosCaracteres = $maiusculas . $minusculas . $numeros . $especiais;
-    $caracteresLength = strlen($todosCaracteres);
+    //todos os conjuntos possiveis para a senha
+    $letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?';
+    
+    //Juntas todos os caracteres em um único conjunto
+    $caracteresLength = strlen($letras);
     $senha = '';
 
     // Seleciona um caractere aleatório a cada iteração
         for ($i = 0; $i < $digitos; $i++) {
         $indiceAleatorio = random_int(0, $caracteresLength - 1);
-         $senha .= $todosCaracteres[$indiceAleatorio];
+         $senha .= $letras[$indiceAleatorio];
 
     }
 
     return $senha;
 
 }
-
+   
     //Senha gerada automaticamente
     $tamanhoDesejado = 10;
     $senhaGerada = gerarSenha($tamanhoDesejado);
