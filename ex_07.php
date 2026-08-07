b48344b53c66841ac49741b87b5d8833e347651e
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-08-07 09:43:33 -0300

    ADD - Adicionei o 8 exercicio

--- ex_07.php
+++ ex_07.php
@@ -1,9 +1,34 @@
 <?php
 
-function  calcularDesconto() {
+function  calcularDesconto($Valor) {
 
+if ($Valor> 1000){
+    $desconto = 0.30;
+} elseif ($Valor >500){
+    $desconto = 0.20;
+}elseif ($Valor > 100){
+    $desconto = 0.20;
+}else {
+    $desconto = 0;
+}
 
+$valor_desconto = $Valor * $desconto;
+$valor_final = $Valor - $valor_desconto;
+
+    return [
+    "Valor_original" => $Valor,
+    "Desconto" => $valor_desconto,
+    "Valor_final" => $valor_final
+    ];
 
 }
+
+$valor_usuario = 670;
+$resultado = calcularDesconto($valor_usuario);
+
+echo "Valor original: R$ " . number_format($resultado["Valor_original"], 2, ",", ".") . "<br>";
+echo "Desconto aplicado: R$ " . number_format($resultado["Desconto"], 2, ",", ".") . "<br>";
+echo "Valor final: R$ " . number_format($resultado["Valor_final"], 2, ",", ".") . "<br>";
+
 
 ?>
